d20ac3a3e36896c1aee4e97670daa7a1a8eca19e
Manish Dhakal <mdhakal3@gsu.edu>
2025-12-04 23:45:11 -0500

    [UPDATE]: 41% model deploy

--- detector/api.py
+++ detector/api.py
@@ -14,8 +14,6 @@ def ping():
 def predict():
     try:
         data = request.get_json()
-        print(data['passcode'])
-        # print(data["sensor_history"], 'FHDFADSFDSKFHSD')
         keys = [
             "accX",
             "accY",
@@ -30,38 +28,77 @@ def predict():
             "magY",
             "magZ",
         ]
-        
 
         # Convert the list of dict to 2d array
         sensor_data = [[entry[key] for key in keys] for entry in data["sensor_history"]]
-        
-        sensor_data_tensor = torch.tensor(sensor_data).reshape(-1, 100, 12).float()
 
-        model = load_model("/Users/snowman/Desktop/codes/PinDetect/detector/cnn_model.pth")
+        sensor_data_tensor = torch.tensor(sensor_data).reshape(-1, 100, 12).float()
+        sensor_data_tensor = normalize_data(sensor_data_tensor)
+        model = load_model("detector/cnn_model.pth")
         with torch.no_grad():
             outputs = model(sensor_data_tensor)
             _, predicted = torch.max(outputs.data, 1)
             predicted_pins = predicted.tolist()
-            
+
         result = {
             "status": "OK",
             "received_data": data,
             "prediction": predicted_pins,
         }
 
-        print(f'Predicted pins is: {predicted_pins} for user pin is {data["passcode"]}')
+        print(
+            f'Predicted pins: {predicted_pins} | Ground truth pins: {"".join(data["passcode"])}'
+        )
 
         return jsonify(result), 200
 
     except Exception as e:
         print(f"Error during prediction: {e}")
         return jsonify({"error": str(e)}), 500
-    
+
+
 def load_model(model_path: str):
     model = CNN_Model(input_channels=12, num_classes=10)  # Adjust num_classes as needed
-    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    model.load_state_dict(torch.load(model_path, map_location=torch.device("cpu")))
     model.eval()
     return model
+
+
+def normalize_data(data: torch.Tensor) -> torch.Tensor:
+    mean = [
+        -0.9730093,
+        4.9200277,
+        7.549436,
+        -0.09853265,
+        -0.09350976,
+        -0.03539513,
+        0.20190482,
+        0.14733204,
+        0.294621,
+        7.96722,
+        -8.366901,
+        -25.473742,
+    ]
+
+    std = [
+        3.1096122,
+        1.723448,
+        2.3250074,
+        0.5229914,
+        1.093255,
+        0.42980716,
+        0.14433116,
+        0.18931694,
+        0.53165317,
+        64.765564,
+        66.07497,
+        19.72449,
+    ]
+    
+    mean = torch.tensor(mean).reshape(1, 1, -1)
+    std = torch.tensor(std).reshape(1, 1, -1)
+    return (data - mean) / ( std + 1e-7)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
--- detector/cnn_model.py
+++ detector/cnn_model.py
@@ -5,21 +5,18 @@ import torch.nn.functional as F
 class CNN_Model(nn.Module):
     def __init__(self, input_channels, num_classes):
         super(CNN_Model, self).__init__()
-        self.conv1 = nn.Conv1d(in_channels=input_channels, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv1d(in_channels=input_channels, out_channels=64, kernel_size=15, stride=1, padding='same')
         self.pool1 = nn.MaxPool1d(kernel_size=2, stride=2)
-        self.conv2 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=10, stride=1, padding='same')
         self.pool2 = nn.MaxPool1d(kernel_size=2, stride=2)
         
-        # Calculate flattened features size dynamically
-        self.flattened_size = 128 * (100 // 2 // 2) # 128 channels * 25 time steps
+        self.flattened_size = 128 * (100 // 2 // 2) 
 
         self.fc1 = nn.Linear(self.flattened_size, 256)
         self.fc2 = nn.Linear(256, num_classes)
 
     def forward(self, x):
-        # x shape: (batch_size, sequence_length, num_features)
-        # Conv1d expects (batch_size, num_features, sequence_length)
-        x = x.permute(0, 2, 1) 
+        x = x.permute(0, 2, 1)
         
         x = F.relu(self.conv1(x))
         x = self.pool1(x)
@@ -33,7 +30,6 @@ class CNN_Model(nn.Module):
     
 
 if __name__ == "__main__":
-    # Example usage
     model = CNN_Model(input_channels=12, num_classes=10)
     model.eval()
     model.load_state_dict(torch.load("cnn_model.pth", map_location=torch.device('cpu')))
